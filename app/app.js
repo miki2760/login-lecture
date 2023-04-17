f08accf8a84a29a4682cff5ff54eef65ff60490c
김선웅 <baechubro@gimseon-ung-ui-MacBookAir.local>
2023-04-18 00:46:24 +0900

    User 모델 구현

--- app/app.js
+++ app/app.js
@@ -11,10 +11,9 @@ const PORT=3000;
 const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
-
 app.use(express.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 무낮가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({ extended: true }));    
